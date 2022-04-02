4a5df1e327dd5c00e3a662262bb46a9ae6948f73
Roman <96354836+Zhoas@users.noreply.github.com>
2022-04-03 01:41:43 +0400

    Update subspace.sh

--- subspace.sh
+++ subspace.sh
@@ -14,46 +14,53 @@ read -p "YOUR_SUBSPACE_NODE_NAME : " SUBSPACE_NODE_NAME
 echo "=+=+=+=+=+=++=+=++=crypton=+=+=+=+=+=++=+=++="
 
 cd $HOME
-sudo apt update && sudo apt upgrade -y
-sudo apt install wget -y
+rm -rf subspace*
 wget -O subspace-node https://github.com/subspace/subspace/releases/download/snapshot-2022-mar-09/subspace-node-ubuntu-x86_64-snapshot-2022-mar-09
 wget -O subspace-farmer https://github.com/subspace/subspace/releases/download/snapshot-2022-mar-09/subspace-farmer-ubuntu-x86_64-snapshot-2022-mar-09
-sudo mv subspace* /usr/local/bin/
-sudo chmod +x /usr/local/bin/subspace*
-
-sudo adduser --system --home=/var/lib/subspace subspace
+chmod +x subspace*
+mv subspace* /usr/local/bin/
 
 echo -e "\e[1m\e[32m7. Creating service...\e[0m"
 
 echo "[Unit]
 Description=Subspace Node
 After=network.target
+
 [Service]
+User=$USER
 Type=simple
-User=subspace
-ExecStart=subspace-node --chain testnet --wasm-execution compiled --execution wasm --bootnodes \"/dns/farm-rpc.subspace.network/tcp/30333/p2p/12D3KooWPjMZuSYj35ehced2MTJFf95upwpHKgKUrFRfHwohzJXr\" --rpc-cors all --rpc-methods unsafe --ws-external --validator --telemetry-url \"wss://telemetry.polkadot.io/submit/ 1\" --name $SUBSPACE_NODE_NAME
+ExecStart=$(which subspace-node) --chain testnet --wasm-execution compiled --execution wasm --bootnodes \"/dns/farm-rpc.subspace.network/tcp/30333/p2p/12D3KooWPjMZuSYj35ehced2MTJFf95upwpHKgKUrFRfHwohzJXr\" --rpc-cors all --rpc-methods unsafe --ws-external --validator --telemetry-url \"wss://telemetry.polkadot.io/submit/ 1\" --name $SUBSPACE_NODE_NAME
 Restart=on-failure
-RestartSec=10
-LimitNOFILE=10000
+LimitNOFILE=65535
+
 [Install]
-WantedBy=multi-user.target" > /etc/systemd/system/subspace-node.service
+WantedBy=multi-user.target" > $HOME/subspaced.service
+
 
 echo "[Unit]
-Description=Subspace Farmer
-Requires=subspace-node.service
+Description=Subspaced Farm
 After=network.target
-After=subspace-node.service
+
 [Service]
+User=$USER
 Type=simple
-User=subspace
-ExecStart=subspace-farmer farm --reward-address=$SUBSPACE_WALLET_ADDRESS
-Restart=always
-RestartSec=10
-LimitNOFILE=10000
+ExecStart=$(which subspace-farmer) farm
+Restart=on-failure
+LimitNOFILE=65535
+
 [Install]
-WantedBy=multi-user.target" > /etc/systemd/system/subspace-farmer.service
+WantedBy=multi-user.target" > $HOME/subspaced-farmer.service
 
-
+mv $HOME/subspaced* /etc/systemd/system/
+sudo systemctl restart systemd-journald
 sudo systemctl daemon-reload
-sudo systemctl enable subspace-node subspace-farmer
-sudo systemctl restart subspace-node subspace-farmer
+sudo systemctl enable subspaced subspaced-farmer
+sudo systemctl restart subspaced
+sudo systemctl restart subspaced-farmer
+echo "=+=+=+=+=+=++=+=++=crypton=+=+=+=+=+=++=+=++="
+echo -e "\e[1m\e[32m Public key : \e[0m"
+subspace-farmer identity view --public-key
+echo "=+=+=+=+=+=++=+=++=crypton=+=+=+=+=+=++=+=++="
+echo -e "\e[1m\e[32m Save your mnemonic : \e[0m"
+subspace-farmer identity view --mnemonic
+echo "=+=+=+=+=+=++=+=++=crypton=+=+=+=+=+=++=+=++="
